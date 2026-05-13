0c00b4f86f52a242133c577b87e3fb9746e82a10
Andy <alex123aze@gmail.com>
2026-05-13 07:59:51 +0200

    fix: Bind DefaultTemplateVariableProvider in Guice and migrate ConsoleCommandSource
    - Add DefaultTemplateVariableProvider binding to MainGuiceModule
    - Fix %PORT% and other template variables not being replaced during deployment
    - Migrate ConsoleCommandSource from PrettyLog to Console.println()

--- api/src/main/kotlin/gg/scala/universe/command/ConsoleCommandSource.kt
+++ api/src/main/kotlin/gg/scala/universe/command/ConsoleCommandSource.kt
@@ -2,20 +2,20 @@ package gg.scala.universe.command
 
 import com.google.inject.Inject
 import com.google.inject.Singleton
-import cz.lukynka.prettylog.LogType
-import cz.lukynka.prettylog.log
+import gg.scala.universe.console.Console
+import gg.scala.universe.console.log
 
 /**
  * The console implementation of [CommandSource].
  *
- * All messages sent to this source are forwarded to the console via [cz.lukynka.prettylog.log].
+ * All messages sent to this source are forwarded to the console via [Console.println].
  * The console is always considered to have full permissions.
  */
 @Singleton
 class ConsoleCommandSource @Inject constructor() : CommandSource {
 
     override fun sendMessage(message: String) {
-        log(message, LogType.INFORMATION)
+        log(message)
     }
 
     override fun sendMessage(vararg messages: String) {
